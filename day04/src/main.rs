36b754295ce7bf601b1d22c1476bec5bdc6ad571
Eric Walker <eric.walker@gmail.com>
2023-09-11 17:59:06 -0600

    Revisit Day 4 implementation after reading through other solutions

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -5,21 +5,18 @@
 //
 // Other solutions:
 //  - https://github.com/Crazytieguy/advent-of-code/blob/master/2021/src/bin/day4/main.rs
-//  - https://www.reddit.com/r/adventofcode/comments/r8i1lq/comment/hn7y21w/
-//  - https://www.reddit.com/r/adventofcode/comments/r8i1lq/comment/hn8q7pt/
+//    Revisit.  Concise solution.  Procedural.
 //  - https://www.reddit.com/r/adventofcode/comments/r8i1lq/comment/hn5zor9/
+//    Concise.  Procedural.
+//  - https://www.reddit.com/r/adventofcode/comments/r8i1lq/comment/hn7y21w/
+//    Similar to this one.
+//  - https://www.reddit.com/r/adventofcode/comments/r8i1lq/comment/hn8q7pt/
+//    Check method mutates state.
 //  - https://www.reddit.com/r/adventofcode/comments/r8i1lq/comment/hn7k99s/
 //
-use color_eyre::{eyre::eyre, Result};
+use color_eyre::Result;
 use itertools::Itertools;
-use nom::{
-    bytes::complete::tag,
-    character::complete::newline,
-    combinator::{all_consuming, map, opt},
-    multi::{many1, separated_list1},
-    sequence::{preceded, terminated, tuple},
-    Finish, IResult,
-};
+use ndarray::{Array2, ArrayBase, Dim, OwnedRepr};
 use std::{
     collections::{HashMap, HashSet, VecDeque},
     io::{self, Read},
@@ -29,27 +26,22 @@ type Int = u16;
 type Pos = (usize, usize);
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
-struct Move(Int);
+struct Draw(Int);
 
 #[derive(Clone, Debug)]
-struct Row([Int; 5]);
-
-impl Row {
-    fn iter(&self) -> impl Iterator<Item = &Int> {
-        self.0.iter()
-    }
-}
-
-#[derive(Clone, Debug)]
-struct Board([Row; 5]);
+struct Board(ArrayBase<OwnedRepr<Int>, Dim<[usize; 2]>>);
 
 impl Board {
-    fn iter(&self) -> impl Iterator<Item = (Pos, &Int)> {
-        self.0.iter().enumerate().flat_map(|(i, row)| {
-            row.iter()
-                .enumerate()
-                .map(move |(j, value)| ((i, j), value))
-        })
+    fn iter(&self) -> impl Iterator<Item = (Pos, Int)> + '_ {
+        self.0
+            .rows()
+            .into_iter()
+            .enumerate()
+            .flat_map(move |(i, row)| {
+                row.into_iter()
+                    .enumerate()
+                    .map(move |(j, &value)| ((i, j), value))
+            })
     }
 }
 
@@ -62,7 +54,7 @@ impl From<&Board> for BoardState {
     fn from(board: &Board) -> Self {
         let map = board
             .iter()
-            .map(|((i, j), &value)| (value, (i, j)))
+            .map(|((i, j), value)| (value, (i, j)))
             .collect::<HashMap<_, _>>();
 
         Self {
@@ -73,7 +65,7 @@ impl From<&Board> for BoardState {
 }
 
 impl BoardState {
-    fn add(&mut self, mv: &Move) {
+    fn add(&mut self, mv: &Draw) {
         if let Some(pos) = self.map.get(&mv.0) {
             self.squares.insert(*pos);
         }
@@ -97,14 +89,14 @@ impl BoardState {
 
 struct Task {
     boards: Vec<Board>,
-    moves: Vec<Move>,
+    draws: Vec<Draw>,
 }
 
 impl Task {
     fn part1(&self) -> Int {
         let mut boards = self.boards.iter().map(BoardState::from).collect_vec();
 
-        for m in &self.moves {
+        for m in &self.draws {
             for board in &mut boards {
                 board.add(m);
 
@@ -125,7 +117,7 @@ impl Task {
             .map(BoardState::from)
             .collect::<VecDeque<_>>();
 
-        for m in &self.moves {
+        for m in &self.draws {
             for _ in 0..boards.len() {
                 let mut board = boards.pop_front().unwrap();
                 board.add(m);
@@ -145,49 +137,28 @@ impl Task {
     }
 }
 
-fn parse_row(s: &str) -> IResult<&str, Row> {
-    map(
-        preceded(
-            opt(tag(" ")),
-            separated_list1(many1(tag(" ")), nom::character::complete::u16),
-        ),
-        |cols| Row(cols.try_into().expect("failed to cast to array")),
-    )(s.trim())
-}
-
-fn parse_moves(s: &str) -> IResult<&str, Vec<Move>> {
-    terminated(
-        map(
-            separated_list1(tag(","), nom::character::complete::u16),
-            |nums| nums.into_iter().map(Move).collect_vec(),
-        ),
-        newline,
-    )(s)
-}
-
-fn parse_boards(s: &str) -> IResult<&str, Vec<Board>> {
-    map(separated_list1(newline, parse_row), |rows| {
-        rows.into_iter()
-            .chunks(5)
-            .into_iter()
-            .map(|rows| rows.collect::<Vec<_>>())
-            .map(|rows| Board(rows.try_into().expect("failed to cast to array")))
-            .collect_vec()
-    })(s)
-}
-
-fn parse_input(s: &str) -> IResult<&str, Task> {
-    map(
-        tuple((parse_moves, newline, parse_boards)),
-        |(moves, _, boards)| Task { moves, boards },
-    )(s)
-}
-
 fn parse(s: &str) -> Result<Task> {
-    let (_s, task) = all_consuming(parse_input)(s)
-        .finish()
-        .or(Err(eyre!("failed to parse input: {}", s)))?;
-    Ok(task)
+    let mut split = s.trim().split("\n\n");
+
+    let draws: Vec<Draw> = split
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|n| Draw(n.parse().unwrap()))
+        .collect();
+
+    let boards = split
+        .map(|board_str| {
+            let board_vec = board_str
+                .split_whitespace()
+                .map(|n| n.parse::<Int>().unwrap())
+                .collect();
+            let array = Array2::from_shape_vec([5, 5], board_vec).unwrap();
+            Board(array)
+        })
+        .collect();
+
+    Ok(Task { draws, boards })
 }
 
 fn main() -> Result<()> {
@@ -213,7 +184,7 @@ mod tests {
     fn parsing() {
         let task = example();
         assert_eq!(task.boards.len(), 3);
-        assert_eq!(task.moves.len(), 27);
+        assert_eq!(task.draws.len(), 27);
     }
 
     #[test]
